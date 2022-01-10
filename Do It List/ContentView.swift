105a3d8f2b8347fb41817b972a613d5446ed7449
Zahra Kermani <zahrapkermani@gmail.com>
2022-01-10 11:18:43 +0100

    Initial commit

---
+++ Do It List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Do It List
+//
+//  Created by Zahra Sadeghipoor on 1/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Do It List/Do_It_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Do_It_ListApp.swift
+//  Do It List
+//
+//  Created by Zahra Sadeghipoor on 1/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Do_It_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
